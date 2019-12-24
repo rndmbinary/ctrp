dc75fad3111c47af598facab3d62f4ef01c73ea9
rndmbinary <howard.darian@gmail.com>
2019-12-24 12:19:15 -0600

    Milestone 2.2

--- install.sh
+++ install.sh
@@ -9,21 +9,23 @@ check_account() {
 
 stage_ctrp() {
 	# Install lots of wares
-	sudo apt-get install -y apt-transport-https snapd neovim zsh git yara radare2 python3-pip p7zip-full less volatility tcpdump lynx host w3m libimage-exiftool-perl software-properties-common ranger whois bind9-host nodejs xpdf libemail-outlook-message-perl
-	sudo snap install powershell --classic
-	sudo curl -L https://www.npmjs.com/install.sh | sh
-	sudo npm install typescript
-	sudo pip3 install yara r2pipe requests scapy bs4 oletools pdfminer.six shodan
+	apt-get install -y apt-transport-https snapd neovim zsh git yara radare2 python3-pip p7zip-full less volatility tcpdump lynx host w3m libimage-exiftool-perl software-properties-common ranger whois bind9-host nodejs xpdf libemail-outlook-message-perl
+	snap install powershell --classic
+	curl -L https://www.npmjs.com/install.sh | sh
+	npm install typescript
+	pip3 install yara r2pipe requests scapy bs4 oletools pdfminer.six shodan
 
 	# Install terminal customization
 	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	sudo git clone https://github.com/janjoswig/Ducula.git ~/.oh-my-zsh/themes/Ducula
-	sudo sed -i 's/ZSH_THEME\="\w+"/ZSH_THEME\="Ducula\/ducula"/g' < ~/.zshrc
-	sudo sed -i 's/plugins\=\(git\)/plugins\=(git git-prompt command-not-found common-aliases encode64 history urltools\)/g' < ~/.zshrc
-	sudo chown $USER ~/.zsh_history
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/nvim/zsh-syntax-highlighting
-	echo 'source ~/.config/nvim/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh' >> ~/.zshrc
+	git clone https://github.com/janjoswig/Ducula.git ~/.oh-my-zsh/custom/Ducula
+	sed -i -e 's/ZSH_THEME\="\w+"/ZSH_THEME\="Ducula\/ducula"/g' ~/.zshrc
+	sed -i -e 's/plugins\=\(git\)/plugins\=(git git-prompt command-not-found common-aliases encode64 history urltools\)/g' ~/.zshrc
+	chown $USER ~/.zsh_history
 	source ~/.zshrc
+
+	# Install neovim customization
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/nvim/zsh-syntax-highlighting
+        echo 'source ~/.config/nvim/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh' >> ~/.zshrc
 
 	# Install sleuthkit
 	wget https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-4.7.0/sleuthkit-4.7.0.tar.gz ~/
@@ -38,10 +40,10 @@ stage_ctrp() {
 
 	# Install osquery
 	export OSQUERY_KEY=1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $OSQUERY_KEY
-	sudo add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
-	sudo apt-get update
-	sudo apt-get install -y osquery
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $OSQUERY_KEY
+	add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
+	apt-get update
+	apt-get install -y osquery
 
 }
 
